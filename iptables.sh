5fe9dc9c93429172dd52298d82e72499270ec5be
Razlite <razulite@gmail.com>
2016-10-11 02:32:34 +0900

    pingを通したり順番変えたりして動くようにした
    Thanks: @suzutan

--- iptables.sh
+++ iptables.sh
@@ -6,6 +6,26 @@ iptables -F
 # ユーザー定義チェインを削除
 iptables -X
 
+# ループバック (自ホストからの通信は許可)
+iptables -A INPUT -i lo -j ACCEPT
+
+# 確立済みの通信は通す
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# ping(icmp)許可
+iptables -I INPUT -p icmp --icmp-type 0 -j ACCEPT
+iptables -I INPUT -p icmp --icmp-type 8 -j ACCEPT
+
+# SSH (XXX)
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport XXX -j ACCEPT
+
+# HTTP (80, 443)
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
+
+# Minecraft (25565)
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 25565 -j ACCEPT
+
 # ポリシーの設定
 # --------- 受信  全て破棄
 iptables -P INPUT DROP
@@ -13,19 +33,3 @@ iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
 # --------- 通過  全て破棄
 iptables -P FORWARD DROP
-
-# ループバック (自ホストからの通信は許可)
-iptables -A INPUT -i lo -j ACCEPT
-
-# 確立済みの通信は通す
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# SSH (XXX)
--A INPUT -m state --state NEW -m tcp -p tcp --dport XXX -j ACCEPT
-
-# HTTP (80, 443)
--A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
--A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
-
-# Minecraft (25565)
--A INPUT -m state --state NEW -m tcp -p tcp --dport 25565 -j ACCEPT
