ce93852817932f093fde1e885254ea25e6f0f84b
Razlite <razulite@gmail.com>
2016-10-25 12:30:02 +0900

    Add: TeamSpeak3

--- iptables.sh
+++ iptables.sh
@@ -26,6 +26,13 @@ iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
 # Minecraft (25565)
 iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 25565 -j ACCEPT
 
+# TeamSpeak3
+iptables -A INPUT -m state --state NEW -m udp -p udp --dport 9987 -j ACCEPT
+# Query
+# iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 10011 -j ACCEPT
+# ファイル転送
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 30033 -j ACCEPT
+
 # ポリシーの設定
 # --------- 受信  全て破棄
 iptables -P INPUT DROP
